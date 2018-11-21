d6936d8c836f9b6300b1dd10a087239ae8756ade
Johan Sundgren <johan.sundgren@hotmail.com>
2018-11-21 15:48:10 +0100

    Added noise functions for terrain

--- js/geometrySetup.js
+++ js/geometrySetup.js
@@ -1,11 +1,18 @@
 
-var radius = THREE.Math.randFloat( 3, 5 );
+var radius = 5;
+var detail  = 4;
 var irregularity = 0.5;
-var geometry = new THREE.IcosahedronGeometry( radius, 4 );
+var geometry = new THREE.IcosahedronGeometry( radius, detail );
 
 var material = new THREE.ShaderMaterial( {
-  vertexShader: document.getElementById( 'vertexShader' ).textContent,
-  fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+    uniforms: {
+        time: {
+          type: "f",
+          value: 0.0
+        }
+    },
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 } );
 
 var model = new THREE.Mesh( geometry, material );
--- js/render.js
+++ js/render.js
@@ -1,8 +1,11 @@
+var start = Date.now();
+
 function animate() {
-   requestAnimationFrame( animate );
-   renderer.render( scene, camera );
-   model.rotation.x += 0.005;
-   model.rotation.y += 0.005;
+    material.uniforms[ 'time' ].value = 0.0025 * ( Date.now() - start);
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    //model.rotation.x += 0.005;
+    //model.rotation.y += 0.005;
 }
 
 animate();
